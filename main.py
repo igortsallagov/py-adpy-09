fe0ca799ea2a59c38c4db4025cb15ba978ba3865
Igor Tsallagov <igor.tsallagov@gmail.com>
2019-02-26 18:26:38 +0300

    Updated methods

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from datetime import datetime
 
 def read_data(csv_file, database):
     with open(csv_file, encoding='utf8') as csvfile:
+        rows_list = list()
         reader = csv.DictReader(csvfile)
         for row in reader:
             row = dict(row)
             row['Цена'] = int(row['Цена'])
             row['Дата'] += '.2019'
             row['Дата'] = datetime.strptime(row['Дата'], '%d.%m.%Y')
-            database.insert_one(row)
+            rows_list.append(row)
+        database.insert_many(rows_list)
 
 
 def find_cheapest(database):
@@ -23,6 +25,7 @@ def find_cheapest(database):
 
 
 def find_by_name(name, database):
+    name = re.escape(name)
     regex = re.compile(name)
     sorted_list = list(database.find({'Исполнитель': regex}).sort('Цена', 1))
     print(f'Найдено мероприятий: {len(sorted_list)}')
@@ -44,8 +47,8 @@ def find_by_date_range(start_date, end_date, database):
 if __name__ == '__main__':
     client = MongoClient()
     db = client.tickets_db
-    ticket = db.ticket
-    read_data('artists.csv', ticket)
-    find_cheapest(ticket)
-    find_by_name('t', ticket)
-    find_by_date_range('01.01.2019', '30.03.2019', ticket)
+    ticket_db = db.ticket
+    read_data('artists.csv', ticket_db)
+    find_cheapest(ticket_db)
+    find_by_name('t', ticket_db)
+    find_by_date_range('01.01.2019', '30.03.2019', ticket_db)
